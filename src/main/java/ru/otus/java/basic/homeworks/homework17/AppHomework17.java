bad2da9fb76fa508158d74e69ba6bedd680d38c2
poltev_dv <poltev_dv@nlmk.com>
2025-03-03 09:30:32 +0300

    Homework17

--- src/main/java/ru/otus/java/basic/homeworks/homework17/AppHomework17.java
+++ src/main/java/ru/otus/java/basic/homeworks/homework17/AppHomework17.java
@@ -4,18 +4,12 @@ public class AppHomework17 {
     public static void main(String[] args) {
 
         PhoneBook phoneBook = new PhoneBook();
-        try {
-            phoneBook.add("John",123456789);
-            phoneBook.add(null,123456789);
-            phoneBook.add("Bob",48151623);
-            phoneBook.add("Bob",48151624);
-            phoneBook.add("Peter",987654);
-            phoneBook.add("Tom",472106);
-            phoneBook.add("Charley",451418234);
-            phoneBook.add("Charley",null);
-        }catch (RuntimeException e){
-            System.err.println(e);
-        }
+        phoneBook.add("John", 123456789);
+        phoneBook.add("Bob", 48151623);
+        phoneBook.add("Bob", 48151624);
+        phoneBook.add("Peter", 987654);
+        phoneBook.add("Tom", 472106);
+        phoneBook.add("Charley", 451418234);
 
         System.out.println(phoneBook.toString());
         System.out.println(phoneBook.containsPhoneNumber(48151624));
--- src/main/java/ru/otus/java/basic/homeworks/homework17/PhoneBook.java
+++ src/main/java/ru/otus/java/basic/homeworks/homework17/PhoneBook.java
@@ -1,37 +1,47 @@
 package ru.otus.java.basic.homeworks.homework17;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class PhoneBook {
-    private Map<Integer, String> phones = new HashMap<>();
+    private Map<String, Set<Integer>> phones = new HashMap<>();
 
     public void add(String name, Integer phoneNumber) throws IllegalArgumentException {
-        if (phoneNumber == null) {
+        if (phoneNumber == null || name == null) {
             throw new IllegalArgumentException("Fields name and phoneNumber must not be empty");
         }
-        this.phones.putIfAbsent(phoneNumber, name);
+        if (phones.containsKey(name)) {
+            phones.get(name).add(phoneNumber);
+        } else {
+            Set<Integer> phones = new HashSet<>();
+            phones.add(phoneNumber);
+            this.phones.putIfAbsent(name, phones);
+        }
+
     }
 
-    public Map<Integer, String> find(String name) throws IllegalArgumentException {
+    public Set<Integer> find(String name) throws IllegalArgumentException {
 
         if (name == null) {
             throw new IllegalArgumentException("Argument \"name\" must not be null");
         }
-        Map<Integer, String> foundPhones = new HashMap<>();
-        for (Map.Entry<Integer, String> phone : phones.entrySet()) {
-            if (phone.getValue().equals(name)) {
-                foundPhones.put(phone.getKey(), phone.getValue());
-            }
-        }
-        return foundPhones;
+        return phones.get(name);
     }
 
     public boolean containsPhoneNumber(Integer phoneNumber) {
         if (phoneNumber == null) {
             throw new IllegalArgumentException("");
         }
-        return this.phones.containsKey(phoneNumber);
+        for (Map.Entry<String, Set<Integer>> element:phones.entrySet()){
+            for(Integer number: element.getValue()){
+                if(number.equals(phoneNumber)){
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
     @Override
