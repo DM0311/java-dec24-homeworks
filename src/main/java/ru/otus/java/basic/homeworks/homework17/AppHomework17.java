161891a21bbc2f939472bcb3ed68d617b78e5ca1
poltev_dv <poltev_dv@nlmk.com>
2025-03-03 09:34:50 +0300

    Homework17

--- src/main/java/ru/otus/java/basic/homeworks/homework17/AppHomework17.java
+++ src/main/java/ru/otus/java/basic/homeworks/homework17/AppHomework17.java
@@ -4,12 +4,18 @@ public class AppHomework17 {
     public static void main(String[] args) {
 
         PhoneBook phoneBook = new PhoneBook();
-        phoneBook.add("John", 123456789);
-        phoneBook.add("Bob", 48151623);
-        phoneBook.add("Bob", 48151624);
-        phoneBook.add("Peter", 987654);
-        phoneBook.add("Tom", 472106);
-        phoneBook.add("Charley", 451418234);
+        try {
+            phoneBook.add("John",123456789);
+            phoneBook.add(null,123456789);
+            phoneBook.add("Bob",48151623);
+            phoneBook.add("Bob",48151624);
+            phoneBook.add("Peter",987654);
+            phoneBook.add("Tom",472106);
+            phoneBook.add("Charley",451418234);
+            phoneBook.add("Charley",null);
+        }catch (RuntimeException e){
+            System.err.println(e);
+        }
 
         System.out.println(phoneBook.toString());
         System.out.println(phoneBook.containsPhoneNumber(48151624));
