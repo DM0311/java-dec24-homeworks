2a44e0308aaf9ffaf36ef437ec8ea9ea5950dd94
poltev_dv <poltev_dv@nlmk.com>
2025-04-17 10:10:47 +0300

    Homework27

--- src/main/java/ru/otus/java/basic/homeworks/homework25/homework_25.sql
+++ src/main/java/ru/otus/java/basic/homeworks/homework25/homework_25.sql
@@ -31,7 +31,8 @@ DROP TABLE if exists otus_homework.questions;
 CREATE TABLE otus_homework.questions (
 	id serial NOT NULL,
 	"text" varchar NOT NULL,
-	CONSTRAINT question_pk PRIMARY KEY (id)
+	CONSTRAINT question_pk PRIMARY KEY (id),
+
 );
 
 DROP TABLE if exists otus_homework.answers;
@@ -53,5 +54,7 @@ CREATE TABLE otus_homework.test_keys (
 	CONSTRAINT test_keys_pk PRIMARY KEY (id),
 	CONSTRAINT test_keys_answers_fk FOREIGN KEY (id_answer) REFERENCES otus_homework.answers(id) ON DELETE CASCADE,
 	CONSTRAINT test_keys_questions_fk FOREIGN KEY (id_question) REFERENCES otus_homework.questions(id) ON DELETE CASCADE,
-	CONSTRAINT test_keys_tests_fk FOREIGN KEY (id_test) REFERENCES otus_homework.tests(id) ON DELETE CASCADE
+	CONSTRAINT test_keys_tests_fk FOREIGN KEY (id_test) REFERENCES otus_homework.tests(id) ON DELETE CASCADE,
+	CREATE INDEX idx_questions_test_id ON otus_homework.questions(id_test),
+    CREATE INDEX idx_answers_question_id ON otus_homework.answers(id_question)
 );
