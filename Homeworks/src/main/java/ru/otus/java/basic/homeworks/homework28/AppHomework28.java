38bfff6fe0239548754829206ebc4717e4c0a073
DM0311 <karapuzoff90@mail.ru>
2025-04-22 09:35:55 +0300

    master update (#21)
    * master update
    
    * master update
    
    ---------
    
    Co-authored-by: poltev_dv <poltev_dv@nlmk.com>

---
+++ Homeworks/src/main/java/ru/otus/java/basic/homeworks/homework28/AppHomework28.java
@@ -0,0 +1,41 @@
+package ru.otus.java.basic.homeworks.homework28;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class AppHomework28 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите абсолютный путь к файлу:");
+        String absPath = scanner.nextLine();
+        File file = new File(absPath);
+        if (!file.exists()) {
+            System.out.println("Указанный файл не найден! - Укажите корректный путь");
+            return;
+        }
+        System.out.println("Введите искомую последовательность символов");
+        String searchPattern = scanner.nextLine();
+        Pattern pattern = Pattern.compile(Pattern.quote(searchPattern));
+
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file, StandardCharsets.UTF_8))) {
+            StringBuilder text = new StringBuilder();
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                text.append(line).append("\n");
+            }
+            Matcher matcher = pattern.matcher(text);
+            int counter = 0;
+            while (matcher.find()){
+                counter++;
+            }
+            System.out.println("Искомая последовательность символов встретилась "+counter+ " раз");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
